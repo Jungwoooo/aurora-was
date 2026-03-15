cd5e21a8980d6a8dd6af753fdc105410368efe74
박정우 <pcn04200@naver.com>
2026-03-15 23:16:37 +0900

    [FEATURE] login 개발중 - member entity 추가

---
+++ src/main/java/com/aurora/aurora_was/member/entity/Member.java
@@ -0,0 +1,42 @@
+package com.aurora.aurora_was.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity // 💡 "Spring아, 이 클래스 보고 MySQL에 테이블 좀 만들어줘!" 라는 마법의 주문
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 숨기기 (안전성 UP)
+public class Member {
+
+    @Id // 기본키 (PK)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 1, 2, 3... 알아서 번호표 뽑기
+    private Long id;
+
+    @Column(nullable = false, unique = true) // 이메일은 필수고, 중복 가입 안 됨!
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // 나중에 꼭 암호화해서 넣어야 합니다!
+
+    @Column(nullable = false, length = 20)
+    private String name; // 이름 (홍길동)
+
+    @Column(nullable = false, length = 20)
+    private String phone; // 전화번호 (010-1234-5678)
+
+    @Column(nullable = false)
+    private String role;
+
+    // 💡 처음 회원가입 할 때 쓸 조립 설명서 (Builder)
+    @Builder
+    public Member(String email, String password, String name, String phone, String role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.role = role;
+    }
+}
